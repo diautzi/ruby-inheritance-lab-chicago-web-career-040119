188af5694145e0ac752f064328c3e4bd91eccce4
Diana Utan <didiutzi@gmail.com>
2021-02-16 23:11:54 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,15 +1,14 @@
 class Student < User
-
-
+  
   def initialize
    @knowledge = []
- end
+  end
 
- def learn(knowledge)
-   @knowledge << knowledge
- end
+  def learn(knowledge)
+    @knowledge << knowledge
+  end
 
- def knowledge
-   @knowledge
- end
+  def knowledge
+    @knowledge
+  end
 end
